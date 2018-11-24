2a404e8cf3284e0df15ca89cba08e19c9ddd62f5
Kaspar Kivistik <kkivis@ttu.ee>
2018-11-24 06:36:27 +0200

    SB-22 data migration

--- src/main/java/ee/noukogu/sneakerwatch/controller/SneakerController.java
+++ src/main/java/ee/noukogu/sneakerwatch/controller/SneakerController.java
@@ -1,12 +1,20 @@
 package ee.noukogu.sneakerwatch.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ee.noukogu.sneakerwatch.model.Sneaker;
 import ee.noukogu.sneakerwatch.service.SneakerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SneakerController {
@@ -24,18 +32,35 @@ public class SneakerController {
     }
 
     @GetMapping("/sneaker/getAll")
-    public @ResponseBody ResponseEntity<?> getAll() {
+    public @ResponseBody
+    ResponseEntity<?> getAll() {
         return new ResponseEntity<>(sneakerService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/sneaker/getBrands")
-    public @ResponseBody ResponseEntity<?> getBrands() {
+    public @ResponseBody
+    ResponseEntity<?> getBrands() {
         return new ResponseEntity<>(sneakerService.getBrands(), HttpStatus.OK);
     }
 
     @DeleteMapping("/sneaker/deleteAll")
-    public @ResponseBody ResponseEntity<?> deleteAll() {
+    public @ResponseBody
+    ResponseEntity<?> deleteAll() {
         sneakerService.deleteAll();
         return new ResponseEntity<>("DELETED ALL SNEAKERS", HttpStatus.OK);
+    }
+
+    @PostMapping("sneaker/getFromJson")
+    public ResponseEntity<?> populateDatabase(MultipartFile file) throws IOException {
+        InputStream fileContent = file.getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(fileContent));
+
+        String json = in.lines().collect(Collectors.joining());
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<Sneaker> sneakers = mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, Sneaker.class));
+
+        sneakers.forEach(sneaker -> sneakerService.add(sneaker));
+        return new ResponseEntity<>("Shoes added successfully", HttpStatus.OK);
     }
 }
--- src/main/java/ee/noukogu/sneakerwatch/model/Sneaker.java
+++ src/main/java/ee/noukogu/sneakerwatch/model/Sneaker.java
@@ -2,6 +2,8 @@ package ee.noukogu.sneakerwatch.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
@@ -14,27 +16,46 @@ import java.util.Map;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Document(indexName = "sneaker")
 public class Sneaker {
 
+    @JsonProperty("name")
     @Id
-    String name;
-    String top;
-    String inspired;
-    String collection;
-    Integer price;
-    String description;
-    String brand;
-    String history;
-    String feature;
-    String style;
-    String additional;
-    String imageUrl;
-    Integer score;
-    List<String> good = null;
-    List<String> bad = null;
+    public String name;
+    @JsonProperty("top")
+    public String top;
+    @JsonProperty("inspired")
+    public String inspired;
+    @JsonProperty("collection")
+    public String collection;
+    @JsonProperty("price")
+    public String price;
+    @JsonProperty("description")
+    public String description;
+    @JsonProperty("brand")
+    public String brand;
+    @JsonProperty("history")
+    public String history;
+    @JsonProperty("feature")
+    public String feature;
+    @JsonProperty("style")
+    public String style;
+    @JsonProperty("additional")
+    public String additional;
+    @JsonProperty("imageUrl")
+    public String imageUrl;
+    @JsonProperty("score")
+    public Integer score;
+    @JsonProperty("good")
+    public List<String> good = null;
+    @JsonProperty("bad")
+    public List<String> bad = null;
     @JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<>();
-
+    @JsonIgnore
+    private String special;
+    @JsonIgnore
+    private String collaboration;
 
 }
