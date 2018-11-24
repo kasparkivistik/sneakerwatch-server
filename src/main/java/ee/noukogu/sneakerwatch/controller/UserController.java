66f847eb7498f227738df70d1e2555e82743d222
Ilja Samoilov <ilja.samoilov@avalanchelabs.ee>
2018-11-24 11:34:06 +0200

    Added random googlefit data

--- src/main/java/ee/noukogu/sneakerwatch/controller/UserController.java
+++ src/main/java/ee/noukogu/sneakerwatch/controller/UserController.java
@@ -1,6 +1,7 @@
 package ee.noukogu.sneakerwatch.controller;
 
 import ee.noukogu.sneakerwatch.model.User;
+import ee.noukogu.sneakerwatch.service.GoogleFitService;
 import ee.noukogu.sneakerwatch.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,9 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    @Resource
+    private GoogleFitService googleFitService;
+
     @PostMapping("/add")
     public ResponseEntity<User> add(@RequestBody User user) {
         return new ResponseEntity<>(userService.add(user), HttpStatus.OK);
@@ -28,6 +32,11 @@ public class UserController {
         } else {
             return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping("/getGoogleFit")
+    public ResponseEntity<?> getGoogleFitData() {
+        return new ResponseEntity<>(googleFitService.getGoogleFitData(), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/ee/noukogu/sneakerwatch/service/GoogleFitService.java
@@ -0,0 +1,22 @@
+package ee.noukogu.sneakerwatch.service;
+
+import ee.noukogu.sneakerwatch.model.GoogleFitData;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class GoogleFitService {
+
+    private List<String> sports = Arrays.asList("Running", "Tennis", "Skate", "Training", "Basketball", "football", "Hiking");
+
+    public GoogleFitData getGoogleFitData() {
+        GoogleFitData googleFitData = new GoogleFitData();
+        Random random = new Random();
+        googleFitData.setMainSport(sports.get(random.nextInt(sports.size())));
+        googleFitData.setTrainingsThisWeek(random.nextInt(7));
+        return googleFitData;
+    }
+}
